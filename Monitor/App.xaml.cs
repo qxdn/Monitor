2e722b69d3a1bbea7932fc9c16ee4e3932ade0f6
钱旭 <1464238196@qq.com>
2020-03-13 17:39:11 +0800

    UI显示

--- Monitor/App.xaml.cs
+++ Monitor/App.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace Monitor
 {
@@ -13,5 +15,48 @@ namespace Monitor
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _taskbar = (TaskbarIcon)FindResource("Taskbar");
+            base.OnStartup(e);
+        }
+
+        private TaskbarIcon _taskbar;
+    }
+
+    public class NotifyIconViewModel
+    {
+        /// <summary>
+        /// 关闭软件
+        /// </summary>
+        public ICommand ExitApplicationCommand
+        {
+            get
+            {
+                return new DelegateCommand { CommandAction = () => Application.Current.Shutdown() };
+            }
+        }
+    }
+
+    public class DelegateCommand : ICommand
+    {
+        public Action CommandAction { get; set; }
+        public Func<bool> CanExecuteFunc { get; set; }
+
+        public void Execute(object parameter)
+        {
+            CommandAction();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return CanExecuteFunc == null || CanExecuteFunc();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
     }
 }
--- Monitor/MainWindow.xaml.cs
+++ Monitor/MainWindow.xaml.cs
@@ -21,9 +21,76 @@ namespace Monitor
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Information Info { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            //不在任务栏显示
+            this.ShowInTaskbar = false;
+            Info = new Information();
+            Task task = new Task(async () => {
+                while (true)
+                {
+                    this.Dispatcher.Invoke(new Action(delegate
+                    {
+                        GetInformation(Info);
+                        CPULoad.Content = Info.CPULoad.ToString("0.00");
+                        CPUTemp.Content = Info.CPUTemp;
+                        MEMLoad.Content = Info.MEMLoad.ToString("0.00");
+                    }));
+                    await Task.Delay(1000);
+                }
+            });
+            task.Start();
+        }
+
+        //获取电脑信息
+        public void GetInformation(Information information)
+        {
+            UpdateVisitor updateVisitor = new UpdateVisitor();
+            Computer computer = new Computer();
+            computer.Open();
+            computer.CPUEnabled = true;
+            computer.RAMEnabled = true;
+            computer.Accept(updateVisitor);
+            for (int i = 0; i < computer.Hardware.Length; i++)
+            {
+                //查找硬件类型为CPU
+                if (computer.Hardware[i].HardwareType == HardwareType.CPU)
+                {
+                    for (int j = 0; j < computer.Hardware[i].Sensors.Length; j++)
+                    {
+                        //找到温度传感器
+                        if (computer.Hardware[i].Sensors[j].SensorType == SensorType.Temperature)
+                        {   
+                            //找到CPU Package
+                            if("CPU Package" == computer.Hardware[i].Sensors[j].Name)
+                            {
+                                information.CPUTemp = computer.Hardware[i].Sensors[j].Value ?? 0;                              
+                            } 
+                        }
+                        else if (computer.Hardware[i].Sensors[j].SensorType == SensorType.Load)
+                        {
+                            //CPU Total负载
+                            if("CPU Total" == computer.Hardware[i].Sensors[j].Name)
+                            {
+                                information.CPULoad = computer.Hardware[i].Sensors[j].Value ?? 0;
+                            }
+                        }
+                    }
+                }else if(computer.Hardware[i].HardwareType == HardwareType.RAM)
+                {
+                    for (int j = 0; j < computer.Hardware[i].Sensors.Length; j++)
+                    {
+                        if (computer.Hardware[i].Sensors[j].SensorType == SensorType.Load)
+                        {
+                            //RAM负载
+                            information.MEMLoad = computer.Hardware[i].Sensors[j].Value ?? 0;
+                        }
+                    }
+                }
+            }
+            computer.Close();
         }
     }
 
@@ -46,4 +113,14 @@ namespace Monitor
 
         public void VisitParameter(IParameter parameter) { }
     }
+
+    public class Information
+    {
+        public float CPULoad { get; set; }
+
+        public float CPUTemp { get; set; }
+
+        public float MEMLoad { get; set; }
+    }
+
 }
